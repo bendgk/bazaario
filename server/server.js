0fbc005b05974ea21891c1458bbf1f09d247058c
bendgk <benkosten@gmail.com>
2020-09-15 13:11:14 -0400

    Add origin policy for localhost on prod server

--- server/server.js
+++ server/server.js
@@ -5,11 +5,16 @@ const port = 3000
 
 //headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://157.245.135.17:8080');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization,Access-Control-Allow-Origin');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+  const allowedOrigins = ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:9000', 'http://157.245.135.17:8080'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
 });
 
 //MongoDB
